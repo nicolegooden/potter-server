22426cb42b49b66a10c44a3a68aa10509c81b14f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-22 09:50:39 -0700

    Create required data for all four houses

---
+++ data/housesData.js
@@ -0,0 +1,56 @@
+const housesData = [
+  {
+    name: 'Gryffindor',
+    headOfHouse: 'Minerva McGonagall',
+    founder: 'Goderic Gryffindor',
+    houseGhost: 'Nearly Headless Nick',
+    mascot: 'Lion',
+    color1: 'Scarlet',
+    color2: 'Gold',
+    value1: 'Courage',
+    value2: 'Bravery',
+    value3: 'Nerve',
+    value4: 'Chivalry'
+  },
+  {
+    name: 'Hufflepuff',
+    headOfHouse: 'Pomona Sprout',
+    founder: 'Helga Hufflepuff',
+    houseGhost: 'The Fat Friar',
+    mascot: 'Badger',
+    color1: 'Yellow',
+    color2: 'Black',
+    value1: 'Hard work',
+    value2: 'Patience',
+    value3: 'Justice',
+    value4: 'Loyalty'
+  },
+  {
+    name: 'Slytherin',
+    headOfHouse: 'Severus Snape',
+    founder: 'Salazar Slytherin',
+    houseGhost: 'The Bloody Baron',
+    mascot: 'Serpent',
+    color1: 'Green',
+    color2: 'Silver',
+    value1: 'Ambition',
+    value2: 'Cunning',
+    value3: 'Leadership',
+    value4: 'Resourcefulness'
+  },
+  {
+    name: 'Ravenclaw',
+    headOfHouse: 'Fillius Flitwick',
+    founder: 'Rowena Ravenclaw',
+    houseGhost: 'The Grey Lady',
+    mascot: 'Eagle',
+    color1: 'Blue',
+    color2: 'Bronze',
+    value1: 'Intelligence',
+    value2: 'Creativity',
+    value3: 'Learning',
+    value4: 'Wit'
+  }
+];
+
+module.exports = housesData;
