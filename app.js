4482cafb9ba61af0a08b8b23ffd69af0068bd8e0
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-22 13:58:52 -0700

    Add route handler for retrieval of specific character

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
   response.send('Potter Server');
 });
 
-app.get("/api/v1/houses", async (req, res) => {
+app.get('/api/v1/houses', async (req, res) => {
   try {
     const houses = await database('houses').select();
     res.status(200).json(houses);
@@ -21,7 +22,7 @@ app.get("/api/v1/houses", async (req, res) => {
   }
 });
 
-app.get("/api/v1/spells", async (req, res) => {
+app.get('/api/v1/spells', async (req, res) => {
   try {
     const spells = await database('spells').select();
     res.status(200).json(spells);
@@ -30,12 +31,27 @@ app.get("/api/v1/spells", async (req, res) => {
   }
 });
 
-app.get("/api/v1/characters", async (req, res) => {
+app.get('/api/v1/characters', async (req, res) => {
   try {
     const characters = await database('characters').select();
     res.status(200).json(characters);
   } catch (e) {
     res.status(500).json({e});
+  }
+});
+
+app.get('/api/v1/characters/:id', async (req, res) => {
+  try {
+    const character = await database('characters').where('_id', req.params.id).select();
+    if (character) {
+      res.status(200).json(character);
+    } else {
+      res.status(404).json({
+        error: `Could not find character with id ${req.params.id}`
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
 
