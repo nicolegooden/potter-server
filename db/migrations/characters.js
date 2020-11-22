8c48a65a5374fc27ccf363738cedcc74ac2c0e3d
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-21 21:54:19 -0700

    Create schema for characters table

---
+++ db/migrations/characters.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('characters', function (table) {
+    table.increments('id').primary();
+    table.string('boggart');
+    table.string('name');
+    table.string('house');
+    table.string('role');
+    table.string('bloodStatus');
+    table.string('species').defaultTo('human');
+    table.string('school').defaultTo('Hogwarts School of Witchcraft and Wizardry');
+    table.boolean('dumbledorsArmy');
+    table.boolean('orderOfThePhoenix');
+    table.boolean('deathEater');
+    table.boolean('ministryOfMagic');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('characters');
+};
