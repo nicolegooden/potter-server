f164fe89274b4f2a50de9f5158a0d273083fb180
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-22 11:43:55 -0700

    Insert all character data for characters table

---
+++ db/seeds/dev/character_seed.js.js
@@ -0,0 +1,28 @@
+const charactersData = require('../../data/charactersData');
+
+const createCharacter = async (knex, character) => {
+  await knex('characters').insert({
+    boggart: character.boggart,
+    name: character.name,
+    house: character.house,
+    role: character.role,
+    species: character.species,
+    bloodStatus: character.bloodStatus,
+    dumbledorsArmy: character.dumbledorsArmy,
+    orderOfThePhoenix: character.orderOfThePhoenix,
+    deathEater: character.deathEater,
+    ministryOfMagic: character.ministryOfMagic
+  });
+};
+
+exports.seed = async (knex) => {
+  try {
+    await knex('characters').del();
+    let allCharacters = charactersData.map(character => {
+      return createCharacter(knex, character);
+    });
+    return Promise.all(allCharacters);
+  } catch (error) {
+    console.log(`Error seeding data: ${error}`);
+  }
+};
